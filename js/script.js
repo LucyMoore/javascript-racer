e0b46b0579940c69d36e3669aef758cbe38b9ba7
Lucy Moore <lucyviolet.moore@gmail.com>
2016-01-21 21:23:46 +1300

    JS file linked to index

---
+++ js/script.js
@@ -0,0 +1,14 @@
+ 
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+var trackLength = 3     //track length is predetermined
+
+
+
+function movePlayer1 {
+ //when the Q key is pressed
+  //move player 1 right by 1 cell
+  // 
+
+ }
