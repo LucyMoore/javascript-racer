47dd163776faf6bf44536687957f39c3203ab013
Lucy Moore <lucyviolet.moore@gmail.com>
2016-01-23 16:18:05 +1300

    p1 and p2 and winner state functioning

--- js/script.js
+++ js/script.js
@@ -1,65 +1,65 @@
-var textValue1 = "";                              //initiate global values
-var textValue2 = "";
-var trackLength = 0;
+var trackLength = 0;                                  //initialize global variables
+var movesP1 = 0;
+var movesP2 = 0;
 
-$( document ).ready(function() {                   //when page is fully loaded script can start
-  trackLength = prompt("set the track length", "");
-  setTrack(trackLength);
+
+$( document ).ready(function() {                      //when page is fully loaded script can start
+  trackLength = prompt("set the track length", "");   //prompt the user to set a track length
+  setTrack(trackLength);                              //call the function that sets the track
  });             
 
-function setTrack(length){
-  for(i=0;i<trackLength;i++){
-    $("#player1_strip").append("<td>");
+function setTrack(length){                            //this is how we set the track lenth
+  for(i=0;i<trackLength;i++){                         //add another cell to the table untill the limit is reached
+    $("#player1_strip").append("<td>");               
     $("#player2_strip").append("<td>");
   }
 };
 
-$(document).keyup(function(){                     //add event listener
-  var keyPressed = event.keyCode;                 //record what key was pressed
-  movePlayer1(keyPressed);                        //pass to each player move function
+$(document).keyup(function(){                         //add event listener
+  var keyPressed = event.keyCode;                     //record what key was pressed
+  movePlayer1(keyPressed);                            //pass to each player move function
   movePlayer2(keyPressed);
 });
 
-function winner(player){
-    window.alert( player +  " WINS!!");
-    location.reload();
-  }
 
   function movePlayer1(keyPressed){                // this is how we move player 1 
-    if(keyPressed === 81) {                        //check if the key pressed was 'q'
+    if(keyPressed === 81) {                         //check if the key pressed was 'q'
+      if(movesP1< trackLength){                       //check current moves are less than the track length
       var x = document.querySelector('.active');   //
-
         if (x) {                                   //if x is a Truthy 
           var nextX = x.nextElementSibling;        //get the next <td> in the table row 
           nextX.setAttribute('class', 'active');    //set the class on the next <td> to "active"
           x.setAttribute('class', '');              //remove the class from the current <td>
+          movesP1++;                                    //increase moves by 1
         }
-      
-        textValue1 = $(".active").text();            // extract the text value of the active class
-        console.log(textValue1)
-        if(textValue1 === "FINISH"){                // if it is the finish line 
-          winner("Player 1");                                 //run the winner function
-        }
-
+      }
+      else{
+        winner("player1");                          //if number of moves is more than the track length print winner
+      }
     }            
   };
 
-  function movePlayer2(keyPressed){         // this is how we move player 2 
-    if(keyPressed === 80) {                 //check if the key pressed was 'p'
-      var x = document.querySelector('.active2'); //
 
-        if (x) {                                    //if x is a Truthy 
-          var nextX = x.nextElementSibling;         //get the next <td> in the table row
+function movePlayer2(keyPressed){                // this is how we move player 1 
+    if(keyPressed === 80) {                         //check if the key pressed was 'q'
+      if(movesP2 < trackLength){                       //check current moves are less than the track length
+      var x = document.querySelector('.active2');   //
+        if (x) {                                   //if x is a Truthy 
+          var nextX = x.nextElementSibling;        //get the next <td> in the table row 
           nextX.setAttribute('class', 'active2');    //set the class on the next <td> to "active"
           x.setAttribute('class', '');              //remove the class from the current <td>
+        movesP2++;                                    //increase moves by 1
         }
-
-        textValue = $(".active2").text();                       // extract the text value of the active class
-        
-        if(textValue === "FINISH"){                             // if it is the finish line 
-          winner("player 2");                                   //run the winner function
-        }
-    }  
+      }
+      else{
+        winner("player 2");                          //if number of moves is more than the track length print winner
+      }
+    }            
   };
-
   
+
+ function winner(player){
+    window.alert( player +  " WINS!!");
+    location.reload();
+  }
+ 
